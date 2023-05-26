25b17f36bff9dc80a78cc43c60b95b4d97db0964
foyzullah <foyz.cse@gmail.com>
2023-05-26 21:32:23 +0600

    classAndObject

--- classAndobject.php
+++ classAndobject.php
@@ -11,12 +11,13 @@ class Employee {
         $this->age       = $age;
         $this->education = $education;
         $this->address   = $address;
-        return 'Name: ' . $name . ', Age: ' . $age . ', Education: ' . $education . ', Address: ' . $address;
+        // return "Name: " . $name . "\nAge:" . $age . "\nEducation: " . $education . ', Address: ' . $address;
+        return "Name: {$name}  \nAge: {$age} \nEducation: {$education} \nAddress: {$address}";
     }
 }
 
-// $foyz = new Employee();
-// $foyz->info( 'Foyzullah', '32', 'Bsc in CSE', 'Satkhira' );
+$foyz = new Employee();
+echo ( $foyz->info( 'Foyzullah', '32', 'Bsc in CSE', 'Satkhira' ) );
 
 class AnotherClass {
     public $name = 'Abdullah';
